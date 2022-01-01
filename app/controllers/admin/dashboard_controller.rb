3f02b5eb0aadb0f4f94e1ccc6bc283346fdf362c
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-01 17:51:45 -0500

    add database queries and erb in dashboard views to implement admin dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    # count of how many products are in the database
+    @num_of_products = Product.count
+    # count of how many categories are in the database
+    @num_of_categories = Category.count
   end
 end
