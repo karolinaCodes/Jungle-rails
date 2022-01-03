3443c843a48d10ab160b464192c52f5aa2e14447
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-03 12:00:18 -0500

    add authentication for all admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  
   def show
     # count of how many products are in the database
     @num_of_products = Product.count
