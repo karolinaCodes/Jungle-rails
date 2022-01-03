e9ab405d2ee86288b354f3d781c0cdb1235b2bd2
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-03 15:06:12 -0500

    add new and create user controler methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+ # render signup form
+  def new
+  end
+
+  #receiving the form and creating a user with the form's parameters
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
