d2550073824a05562cd72d8f456c31822cad8817
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-10 22:03:13 -0500

    some minor changes like commenting and reformatting while trying to find signup bug- no bug found: forgot password requirement of 8 char

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
- # render signup form
   def new
   end
 
-  # receiving the form info and creating a user with the form's parameters
   def create
     user = User.new(user_params)
     if user.save
-      puts user
       session[:user_id] = user.id
       redirect_to '/'
     else
